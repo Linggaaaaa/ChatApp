9fd79744deb39a4e79caa10bcc7c5372b1e59114
Linggaaaaa <129972002+Linggaaaaa@users.noreply.github.com>
2024-05-06 20:52:53 +0700

    Menambahkan file ChatApp

---
+++ ChatApp/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Barang Rongsok</title>
+    <link rel="stylesheet" href="Tampilan.css">
+    <link rel="icon" href="https://img.freepik.com/premium-vector/garbage-bin-icon-comic-style-recycle-cartoon-vector-illustration-white-isolated-background-trash-basket-splash-effect-sign-business-concept_157943-15737.jpg?w=826">
+    <style>
+</style>
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><a href="">Pengumpulan Barang Rongsok</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="signup.php" class="tbl-biru">Signup</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper-content wrapper">
+        <!-- untuk home -->
+        <section id="home">
+            <img src="https://img.freepik.com/premium-vector/garbage-bin-icon-comic-style-recycle-cartoon-vector-illustration-white-isolated-background-trash-basket-splash-effect-sign-business-concept_157943-15737.jpg?w=826"/>
+            <div class="kolom">
+                <p class="deskripsi">Barang Rongsok</p>
+                <h2 class="judul">Rongsok Berkualitas, Harga Istimewa!</h2>
+                <h3 class="deskripsi">Barang rongsok adalah limbah bahan yang sudah tidak seperti wujud aslinya. Namun masih bisa anda gunakan untuk konstruksi. Khusunya untuk pekerjaan non struktural. Baik pada proyek itu juga atau untuk proyek-proyek yang lain. Material rongsok misalnya potongan besi, balok kayu, plastik, kaca, kardus. </h3>
+            </div>
+        </section>
+    </div>
+
+    <div id="contact">
+        <a class="social-icon" href="https://www.facebook.com/lingga.alzidan/">
+            <ion-icon name="logo-facebook"></ion-icon>
+        </a>
+
+        <a class="social-icon" href="https://api.whatsapp.com/send?phone=6285774011530">
+            <ion-icon name="logo-whatsapp"></ion-icon>
+        </a> 
+
+        <a class="social-icon" href="https://mail.google.com/mail/u/0/?hl=en#inbox">
+            <ion-icon name="mail-outline"></ion-icon>
+        </a>
+        
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</div>
+
+</body>
+</html>
